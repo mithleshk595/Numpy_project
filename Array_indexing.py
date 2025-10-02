5aee28ade866f4d87196348e2272b7d34f62b7a7
Mithleshk595 <mithleshk0982@gmail.com>
2025-10-02 11:17:36 +0530

    Added Array_indexing.py

---
+++ Array_indexing.py
@@ -0,0 +1,28 @@
+# import numpy as np
+# arr = np.array([1, 2, 3, 4])
+# print(arr[0])
+
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4])
+# print(arr[1])
+
+
+# import numpy as np
+# arr = np. array([1, 2, 3, 4])
+# print(arr[2] + arr[3])
+
+
+# import numpy as np
+# arr = np.array([[1, 2, 3 ,4, 5], [6, 7, 8, 9,10]])
+# print("second element on first row: ", arr[0, 1])
+
+
+import numpy as np
+arr = np. array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]])
+print(arr[0, 1, 2])
+
+
+
+
+
