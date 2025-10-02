77408e285bbc739d1653b5e56683d7d8efeb816f
Mithleshk595 <mithleshk0982@gmail.com>
2025-10-02 17:45:21 +0530

    Added_slicing.py

---
+++ Array_Slicing.py
@@ -0,0 +1,24 @@
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr[1:4])
+
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[4:])
+
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[:4])
+
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[-3:-1])
+
+
+import numpy as np 
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5:2])
+
