a18512c4e82d8ba5a9bf96e80351f562c00d938d
Mithleshk595 <mithleshk0982@gmail.com>
2025-10-04 21:58:14 +0530

    Added Array_iterating.py

---
+++ Array_iterating.py
@@ -0,0 +1,47 @@
+import numpy as np 
+
+arr = np.array([1, 2, 3])
+
+for x in arr:
+    print(x)
+
+
+import numpy as np
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in arr:
+    print(x)
+
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in arr:
+    for y in x:
+        print(y)
+
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6],[7, 8, 9], [10, 11, 12]]])
+for x in arr:
+    print(x)
+
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]])
+for x in arr:
+    for y in x:
+        for z in y:
+            print(z)
+    
+
+import numpy as np
+
+arr = np.array([[[1, 2], [3, 4], [5, 6], [7 ,8]]])
+for x in np.nditer(arr):
+    print(x)
+
+
