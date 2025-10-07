8a9dac597af10867809ee14ce864d04a3016bc42
Mithleshk595 <mithleshk0982@gmail.com>
2025-10-07 18:07:21 +0530

    Adde Array_sort.py

---
+++ Array_sort.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+
+import numpy as np 
+
+arr = np.array(["Banana", "Cherry", "Apple"])
+print(np.sort(arr))
+
+
+import numpy as np
+
+arr = np.array([True, False, True])
+print(np.sort(arr))
+
+
+import numpy as np 
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+print(np.sort(arr))
