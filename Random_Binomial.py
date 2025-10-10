fd93b810cb1f4a9b976e0e887185a3c9651be111
Mithleshk595 <mithleshk0982@gmail.com>
2025-10-10 18:59:16 +0530

    Added Random_Binomial.py

---
+++ Random_Binomial.py
@@ -0,0 +1,22 @@
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.binomial(n=10, p=0.5, size=10))
+plt.show()
+
+
+
+
+from numpy import random
+import matplotlib.pylab as plt
+import seaborn as sns
+
+data = {
+    "normal" : random.normal(loc= 50, scale=5, size=1000),
+    "binomial": random.binomial(n=100, p=0.5, size=1000)
+
+}
+
+sns.displot(data, kind="kde")
+plt.show()
